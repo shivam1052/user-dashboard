5af5ec8db13d16d576b238a11f5483370fff637c
Shivam Sharma <iamshivam152@gmail.com>
2026-03-14 22:40:07 +0530

    working on css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,14 +9,41 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <h2>Username</h2>
+    <div className="p-6 grid md:grid-cols-3 gap-4">
+      {/* user1 */}
+      <div className="border p-4 rounded shadow">
+        <h2 className="font-bold text-lg">Username</h2>
         <p>User Email</p>
         <p>Comapny name</p>
         <p>User address</p>
 
-        <button>View Details</button>
+        <button className="mt-2 bg-blue-500 text-white px-3 py-1 rounded">
+          View Details
+        </button>
+      </div>
+
+      {/* user2 */}
+      <div className="border p-4 rounded shadow">
+        <h2 className="font-bold text-lg">Username</h2>
+        <p>User Email</p>
+        <p>Comapny name</p>
+        <p>User address</p>
+
+        <button className="mt-2 bg-blue-500 text-white px-3 py-1 rounded">
+          View Details
+        </button>
+      </div>
+
+      {/* user3 */}
+      <div className="border p-4 rounded shadow">
+        <h2 className="font-bold text-lg">Username</h2>
+        <p>User Email</p>
+        <p>Comapny name</p>
+        <p>User address</p>
+
+        <button className="mt-2 bg-blue-500 text-white px-3 py-1 rounded">
+          View Details
+        </button>
       </div>
     </div>
   );
